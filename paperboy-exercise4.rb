4b3e9f22a55e6912b4a6a5d4cb45f29417214bd8
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-05-29 13:56:46 -0400

    added .abs and made .deliver function dryer

--- paperboy-exercise4.rb
+++ paperboy-exercise4.rb
@@ -11,18 +11,16 @@ class Paperboy
   end
 
   def deliver(start_add, end_add)
-    houses = (end_add - start_add)
-    if houses == quota
+    houses = (end_add - start_add).abs
+    if houses >= quota
       @earnings += (0.25 * quota)
-      total = (0.25 * quota)
-    elsif houses < quota
+      @earnings += (0.5 * (houses - quota))
+      total = ((0.25 * quota) + (0.5 * (houses - quota)))
+    else
       @earnings -= 2
       total = -2
-    else
-      @earnings += ((0.25 * quota) + (0.5 * (houses - quota)))
-      total = ((0.25 * quota) + (0.5 * (houses - quota)))
     end
-    @experience += (end_add - start_add)
+    @experience += houses
     return total
   end
 
@@ -44,6 +42,13 @@ p kevin.performance
 p "------------------"
 p "Deliver Address 1 - 51"
 tot = kevin.deliver(1, 51)
+p kevin
+p tot
+p "New quota: #{kevin.quota}"
+p kevin.performance
+p "------------------"
+p "Deliver Address 300 - 100"
+tot = kevin.deliver(300, 100)
 p kevin
 p tot
 p "New quota: #{kevin.quota}"
