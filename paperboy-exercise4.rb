4bc7431d120499ad1a68d71afc919ab051ada13a
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-05-29 13:08:22 -0400

    completed exercise 4

---
+++ paperboy-exercise4.rb
@@ -0,0 +1,50 @@
+class Paperboy
+
+  def initialize(name)
+    @name = name
+    @experience = 0
+    @earnings = 0
+  end
+
+  def quota
+    50 + (@experience/2)
+  end
+
+  def deliver(start_add, end_add)
+    houses = (end_add - start_add)
+    if houses == quota
+      @earnings += (0.25 * quota)
+      total = (0.25 * quota)
+    elsif houses < quota
+      @earnings -= 2
+      total = -2
+    else
+      @earnings += ((0.25 * quota) + (0.5 * (houses - quota)))
+      total = ((0.25 * quota) + (0.5 * (houses - quota)))
+    end
+    @experience += (end_add - start_add)
+    return total
+  end
+
+
+  def performance
+    "I'm #{@name}, and I have delivered #{@experience} papers, and have made $#{@earnings}"
+  end
+end
+
+kevin = Paperboy.new("Kevin")
+p kevin
+p kevin.quota
+p "Deliver Address 1 - 51"
+tot = kevin.deliver(1, 51)
+p kevin
+p tot
+p "New quota: #{kevin.quota}"
+p kevin.performance
+p "------------------"
+p "Deliver Address 1 - 51"
+tot = kevin.deliver(1, 51)
+p kevin
+p tot
+p "New quota: #{kevin.quota}"
+p kevin.performance
